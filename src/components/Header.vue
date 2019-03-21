47de50510406e68669c8bf69ca72792b03200720
Johann Ingi <joi.ingi@me.com>
2019-03-21 17:21:00 +0100

    Animation and BEM

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,10 +5,12 @@
        </div>
 
       <div class="main-header__intro-box">
-         <h1 class="main-header__heading-primary">
-          <span class="main-header__heading-primary-main">Welcome to johanningi.com</span>
-          <span class="main-header__heading-primary-sub">My Personal Website</span>
+         <h1 class="heading-primary">
+          <span class="heading-primary--main">Welcome to johanningi.com</span>
+          <span class="heading-primary--sub">My Personal Website</span>
        </h1>
+
+       <a href="#" class="btn btn--white btn--animated">Enter</a>
       </div>
     </header>
 </template>
@@ -50,18 +52,19 @@ export default {
    top: 50%;
    left: 50%;
    transform: translate(-40%, -50%);
+   text-align: center;
 }
 
-.main-header__heading-primary {
+.heading-primary {
    color: #fff;
    text-transform: uppercase;
-   text-align: center;
+   margin-bottom: 3rem;
 
    // For smooter animation
    backface-visibility: hidden;
 }
 
-.main-header__heading-primary-main {
+.heading-primary--main {
    display: block;
    font-size: 2rem;
    font-weight: 400;
@@ -73,7 +76,7 @@ export default {
    animation-timing-function: ease-in-out;
 }
 
-.main-header__heading-primary-sub {
+.heading-primary--sub {
    display: block;
    font-size: 6rem;
    font-weight: 400;
@@ -113,6 +116,73 @@ export default {
       opacity: 1;
       transform: translate(0);
    }
+}
+
+@keyframes moveInBottom {
+   0% {
+      opacity: 0;
+      transform: translateY(50px);
+   }
+
+   100%{
+      opacity: 1;
+      transform: translateY(0);
+   }
+}
+
+// Button
+.btn:link,
+.btn:visited {
+   position: relative;
+   text-transform: uppercase;
+   text-decoration: none;
+   padding: 1.5rem 4rem;
+   display: inline-block;
+   border-radius: 10rem;
+   transition: all .2s;
+
+   &:hover {
+      transform: translateY(-.3rem);
+      box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+   }
+
+   &:active {
+      transform: translateY(-.1rem);
+      box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+   }
+}
+
+.btn--white {
+   background-color: #fff;
+   color: #777;
+}
+
+// animate fake btn behind the button
+.btn::after {
+   content: "";
+   display: inline-block;
+   height: 100%;
+   width: 100%;
+   border-radius: 100px;
+   position: absolute;
+   top: 0;
+   left: 0;
+   z-index: -1;
+   transition: all .4s;
+}
+
+.btn--white::after {
+   background-color: #fff;
+}
+
+.btn:hover::after {
+   transform: scaleX(2) scaleY(1.5);
+   opacity: 0;
+}
+
+.btn--animated {
+   animation: moveInBottom .4s ease-out .75s;
+   animation-fill-mode: backwards;
 }
 
 </style>
